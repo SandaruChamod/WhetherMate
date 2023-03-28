078005be7918430e2090dab78b65b6465b81b7a9
SandaruChamod <sandaruchamod97@gmail.com>
2023-03-28 22:48:03 +0800

    Starter Project

--- WhetherMate/WhetherMateApp.swift
+++ WhetherMate/WhetherMateApp.swift
@@ -1,17 +1,19 @@
 //
-//  WhetherMateApp.swift
-//  WhetherMate
+//  CourseWork2StarterApp.swift
+//  CourseWork2Starter
 //
-//  Created by user234080 on 3/28/23.
+//  Created by GirishALukka on 16/03/2023.
 //
 
 import SwiftUI
 
 @main
 struct WhetherMateApp: App {
+    @StateObject var modelData = ModelData()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
