421cc0328e453e1aef78e9b2b985a595ad65863d
SandaruChamod <sandaruchamod97@gmail.com>
2023-03-28 22:38:22 +0800

    Initial Commit

---
+++ WhetherMate/WhetherMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhetherMateApp.swift
+//  WhetherMate
+//
+//  Created by user234080 on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct WhetherMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
